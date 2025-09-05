b3f9067697f7089846f42aa69c0ca8f4151966a8
mohammademon393 <mdemon357ep@gmail.com>
2025-09-05 22:18:47 +0600

    dinamic the lesson button

--- script/index.js
+++ script/index.js
@@ -10,11 +10,36 @@ const wordLesson =(id)=>{
     const url = `https://openapi.programming-hero.com/api/level/${id}`
     fetch(url)
     .then(res => res.json())
-    .then(data => displayWord(data))
+    .then(data => displayWord(data.data))
 }
 
 const displayWord =(words) => {
-    console.log(words);
+    const wordContainer = document.getElementById('word-container');
+    wordContainer.innerHTML = "";
+
+    words.forEach(word => {
+        const card = document.createElement('div');
+        card.innerHTML = `
+        
+            <div class="card card-dash bg-base-100 w-96 text-center">
+                <div class="card-body space-y-4">
+                    <h2 class=" text-center text-2xl font-bold">${word.word}</h2>
+                    <p class="font-semibold">Meaning /Pronounciation</p>
+                    <span class="font-bangla text-2xl  ">${word.meaning} / ${word.pronunciation}"</span>
+                    <div class="flex justify-between">
+                        <button class="btn btn-outline btn-primary rounded-full"><i class="fa-solid fa-circle-info"></i>
+                        </button>
+
+                        <button class="btn btn-outline btn-primary rounded-full"><i
+                        class="fa-solid fa-volume-high"></i></button>
+                    </div>
+                </div>
+            </div>
+
+        `;
+        wordContainer.append(card);
+        
+    });
     
 }
 
